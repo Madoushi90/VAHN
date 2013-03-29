9fec567d45461305363e0a74199cc87536e24b8a
Justin Brewer <jzb0012@auburn.edu>
2013-03-28 20:31:22 -0500

    Node stops properly

--- src/node.c
+++ src/node.c
@@ -1,10 +1,18 @@
 #include <stdlib.h>
+#include <signal.h>
 #include <sahn.h>
 #include <pthread.h>
 
+pthread_mutex_t lock;
+pthread_cond_t wait;
+
+void shutdown(int sig){
+  pthread_cond_broadcast(&wait);
+}
+
 int main(int argc, char** argv){
-  pthread_mutex_t lock;
-  pthread_cond_t wait;
+  signal(SIGINT,&shutdown);
+
   pthread_mutex_init(&lock,NULL);
   pthread_cond_init(&wait,NULL);
 
